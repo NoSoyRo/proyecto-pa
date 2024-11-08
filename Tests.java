1df09bb8987d958acc58c2175107353d53f2f220
Rodrigo <rodri.mo@ciencias.unam.mx>
2024-11-08 13:53:54 -0600

    Added method to Archivo...Buffered the method getDimensiones to retreive the demansions of the CSV.

--- Tests.java
+++ Tests.java
@@ -9,12 +9,19 @@ public class Tests {
         archivo.copiar("covid.csv");
     }
 
-    public void testArchivoCopiaTextoBufferedCopiaOrigenDestino() {
-
+    public void testArchivoObtenDimesniones() {
+        ArchivoCopiaTextoBuffered archivo = new ArchivoCopiaTextoBuffered();
+        int[] arr = archivo.getDimensiones("covid.csv");
+        String resView = Integer.toString(arr[0]) + ',' + Integer.toString(arr[1]);
+        System.out.println(resView);
+        // works correctly
     }
 
     public static void main(String[] args) {
         Tests tests = new Tests();
+        System.out.println("\n Unit test 1: testArchivoCopiaTextoBufferedCopiaOrigen \n");
         tests.testArchivoCopiaTextoBufferedCopiaOrigen();
+        System.out.println("\n Unit test 2: testArchivoObtenDimesniones \n");
+        tests.testArchivoObtenDimesniones();
     }
 }
--- src/Models/ArchivoCopiaTextoBuffered.java
+++ src/Models/ArchivoCopiaTextoBuffered.java
@@ -107,5 +107,47 @@ public class ArchivoCopiaTextoBuffered extends Archivo {
             imprimirSalida(nombreArchivoOrigen, nombreArchivoDestino, tiempoInicio, tiempoFin);
         }
     }
+    // Regresa un arreglo con dos entradas: el primer elemento nos dice la cantidad de registros y la segunda me dice la cantidad de columnas
+    public int[] getDimensiones(String origen){
+        nombreArchivoOrigen = origen;
+        objetoFile_archivoOrigen = new File(RUTA_DEFAULT, nombreArchivoOrigen);
+        if (!verificarExistencia(objetoFile_archivoOrigen)) {
+            System.out.printf(" %s \n", nombreArchivoOrigen);
+            return new int[] {-1,-1};
+        }
+        int cuentaLineas = 0;
+        int cuentaCampos = 0;
+        int[] arr = new int[] {cuentaLineas, cuentaCampos};
+        try {
+            archivoOrigen = new BufferedReader(new FileReader(objetoFile_archivoOrigen));
+            tiempoInicio = System.nanoTime();
+            /* Lee el contenido del archivoOrigen y lo escribe en archivoDestino */
+            
+            while ((linea = archivoOrigen.readLine()) != null) {
+                if (cuentaLineas == 0) {
+                    campos = linea.split(SEPARADOR_COLUMNA_DEFAULT);
+                    cuentaCampos = campos.length;
+                }
+                cuentaLineas = cuentaLineas + 1;
+                /* Copia TODO el archivo igual, descomente la sig linea si deseo observar */
+                // archivoDestino.write(linea + "\n");
+                /* Copia campos seleccionados, descomente las sig 2 lineas si deseo observar */
+                // campos = linea.split(SEPARADOR_COLUMNA_DEFAULT);
+                // archivoDestino.write(campos[0] + "," + campos[1] + "," + campos[2] + "," +
+                // campos[3] + "\n");
+            }
+            tiempoFin = System.nanoTime();
+            archivoOrigen.close();
+            arr = new int[] {cuentaLineas, cuentaCampos};
+            
+
+        } catch (IOException e) {
+            System.err.println(e);
+        } finally {
+            imprimirSalida(nombreArchivoOrigen, nombreArchivoDestino, tiempoInicio, tiempoFin);
+            return arr;
+        }
+
+    }
 
 }
